ef9c0a4f4abcd20f98738a54cdaad6129d742ccb
sanchjain <54800651+sanchjain@users.noreply.github.com>
2020-01-25 17:34:43 +0530

    Add files via upload

---
+++ instabot.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from time import sleep
+from secrets import pw
+
+class InstaBot:
+    def __init__(self, username, password):
+        self.username = username
+        self.driver = webdriver.Chrome(executable_path='/Users/sanchitjain/Downloads/chromedriver')
+        self.driver.get("https://www.instagram.com/?hl=en")
+        sleep(5)
+        self.driver.find_element_by_xpath("//a[contains(text(), 'Log in')]").click()
+        sleep(5)
+        self.driver.find_element_by_xpath("//input[@name = \'username\']").send_keys(username)
+        self.driver.find_element_by_xpath("//input[@name = \'password\']").send_keys(password)
+        self.driver.find_element_by_xpath("//button[@type = 'submit']").click()
+        sleep(5)
+        self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]").click()
+        sleep(10)
+
+    # def unfollowers(self):
+    #     self.driver.find_element_by_xpath("//a[contains(@href, '/{}')]".format(self.username)).click()
+    #     self.driver.find_element_by_xpath("//a[contains(@href, '/Following')]").click()
+
+
+
+
+bot = InstaBot("_sanchitj_", "sancharc")
+# bot.unfollowers()
